4ead1f606d0d863ceb677b77c5314cb88361a4b8
Victor King'e <vinge1718@gmail.com>
2017-08-18 14:22:21 +0300

    add the innitial deposit and name constructor

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,20 @@
-$(document).ready(function(){
+//BACKEND LOGIC
+function Client(name, innitialDeposit){
+  this.name = name[];
+  this.innitialDeposit = innitialDeposit;
+}
 
+
+//FRONT END LOGIC
+$(document).ready(function(){
+  $("form#clientDetails").submit.function(event){
+    event.preventDefault();
+
+    var clientName = $("input#clientName").val();
+    var innitialDeposit = parseInt($("input#innitialDeposit").val());
+    var client = new Client(clientName, innitialDeposit);
+
+
+
+  }
 });
