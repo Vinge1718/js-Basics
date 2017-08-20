d7dc109525daba352260a86c10ea49cbb4470853
Victor King'e <vinge1718@gmail.com>
2017-08-20 22:12:36 +0300

    add the necessary buttons to make the accounnt functional

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,48 @@
 //BACKEND LOGIC
-function Client(name, innitialDeposit){
-  this.name = name[];
-  this.innitialDeposit = innitialDeposit;
+function Client(name, initialDeposit){
+  this.name = name;
+  this.initialDeposit = initialDeposit;
 }
 
 
 //FRONT END LOGIC
 $(document).ready(function(){
-  $("form#clientDetails").submit.function(event){
+  $("form#clientDetails").submit(function(event){
     event.preventDefault();
 
     var clientName = $("input#clientName").val();
-    var innitialDeposit = parseInt($("input#innitialDeposit").val());
-    var client = new Client(clientName, innitialDeposit);
+    var initialDeposit = parseInt($("input#initialDeposit").val());
+
+    var client = new Client(clientName, initialDeposit);
+
+    $("#customer").empty().prepend(clientName)
+    $("#currentBalance span").empty().text(client.initialDeposit);
+    $("#customerDetails, #currentBalance").show();
+
+    $("input#clientName").val("");
+    $("input#initialDeposit").val("");
 
 
+  });
 
-  }
+  $("form#depositForm").submit(function(event){
+    event.preventDefault();
+
+    var depositAmount = parseInt($("input#depositAmount").val());
+    currentAmount = parseInt($("#initialBalance").text());
+
+    $("#initialBalance").text(currentAmount + depositAmount);
+    $("input#depositAmount").val("");
+    $("#customerDetails, #currentBalance").show();
+  });
+
+  $("form#withdrawForm").submit(function(event){
+    event.preventDefault();
+
+    var withdrawnAmount = parseInt($("input#withdrawAmount").val());
+    currentAmount = parseInt($("#initialBalance").text());
+    $("#initialBalance").text(currentAmount - withdrawnAmount);
+    $("input#withdrawAmount").val("");
+    $("#customerDetails, #currentBalance").show();
+  });
 });
